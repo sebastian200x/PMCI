4a1597cedf5c71e5436e627f97c5bf98cfad9638
sebastian200x <jetsebastian4@gmail.com>
2024-03-20 08:37:30 +0800

    update

--- header.php
+++ header.php
@@ -3,12 +3,10 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" href="./styles/images/favicon.ico" type="image/x-icon">
-
 <?php
 require './admin/functions.php';
-
 $currentPage = basename($_SERVER['PHP_SELF']);
-    ?>
+?>
 <div class="top">
     <header>
         <div class="head-container">
@@ -20,7 +18,6 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                     var sidebar = document.getElementById("sidebar");
                     sidebar.classList.toggle("open-sidebar");
                 }
-
                 function closeSidebar() {
                     var sidebar = document.getElementById("sidebar");
                     sidebar.classList.remove("open-sidebar");
@@ -37,26 +34,22 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 <a href="./news.php">NEWS</a>
                 <a href="./contact.php">CONTACT US</a>
             </div>
-
             <h2>PHILIPPINE MALABON CULTURAL INSTITUTE</h2>
             <img class="header-logo" src="./styles/images/PMCI-LOGO.png" alt="" draggable="false">
             <a href="./ADMIN/index.php"></a>
-
         </div>
-
     </header>
-
     <nav>
-
         <div class="nav-container">
             <div class="img-logo">
                 <img src="./styles/images/PMCI-LOGO.png" alt="">
                 <span>&nbsp菲律賓馬拉邦文化學院</span>
             </div>
-
             <div class="navi">
-                <a class="hover <?php echo ($currentPage == 'index.php') ? 'active' : ''; ?>" href="./index.php">Home</a>
-                <ul class="hover name <?php echo ($currentPage == 'k12.php' || $currentPage == 'facilities.php' || $currentPage == 'policies.php') ? 'active' : ''; ?>">
+                <a class="hover <?php echo ($currentPage == 'index.php') ? 'active' : ''; ?>"
+                    href="./index.php">Home</a>
+                <ul
+                    class="hover name <?php echo ($currentPage == 'k12.php' || $currentPage == 'facilities.php' || $currentPage == 'policies.php') ? 'active' : ''; ?>">
                     <li class="menu-item">
                         <a href="#" class="menu-link">Academics</a>
                         <ul class="drop-menu">
@@ -72,18 +65,13 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                         </ul>
                     </li>
                 </ul>
-
-                <a class="hover <?php echo ($currentPage == 'about.php') ? 'active' : ''; ?>" href="./about.php">About</a>
+                <a class="hover <?php echo ($currentPage == 'about.php') ? 'active' : ''; ?>"
+                    href="./about.php">About</a>
                 <a class="hover <?php echo ($currentPage == 'news.php') ? 'active' : ''; ?>" href="./news.php">News</a>
-                <a class="hover <?php echo ($currentPage == 'contact.php') ? 'active' : ''; ?>" href="./contact.php">Contact Us</a>
-
-
-
+                <a class="hover <?php echo ($currentPage == 'contact.php') ? 'active' : ''; ?>"
+                    href="./contact.php">Contact Us</a>
             </div>
-
         </div>
     </nav>
-
 </div>
-
 <main>
